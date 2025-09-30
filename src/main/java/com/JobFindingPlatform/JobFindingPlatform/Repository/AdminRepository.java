6874f76f5b4811ea687358b46c2c995d29428a62
harshithaneelgar <neelgarharshitha@gmail.com>
2025-09-30 15:16:40 +0530

    Repositories

---
+++ src/main/java/com/JobFindingPlatform/JobFindingPlatform/Repository/AdminRepository.java
@@ -0,0 +1,13 @@
+package com.JobFindingPlatform.JobFindingPlatform.Repository;
+
+import com.JobFindingPlatform.JobFindingPlatform.Entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+    List<Admin> findByAdminId(Long adminId);
+    List<Admin> findByUserId(Long userId);
+}
