8796c3ededf9193260449967a19fc6b8dbc5a7f0
harshithaneelgar <neelgarharshitha@gmail.com>
2025-09-30 15:22:04 +0530

    all src files update

---
+++ src/main/java/com/JobFindingPlatform/JobFindingPlatform/JobFindingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.JobFindingPlatform.JobFindingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFindingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFindingPlatformApplication.class, args);
+	}
+
+}
